0e7820c1320a91e51b9e241310e30977a7a1bd3d
Curious Team <curiousdevelopers.in@gmail.com>
2025-03-06 16:50:42 +0530

    folder structured

--- Frontend/src/pages/JSX_Root.jsx
+++ Frontend/src/pages/JSX_Root.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import ResponsiveCardDesign from "../component/JSX_Projects/01. Responsive_Card";
-import Profile from "../component/JSX_Projects/03. FindMentorUi";
-import JSXAsync_Await from "../component/JSX_Concepts/01. JSX_Async_Await";
-import JSX_Promises_Example from "../component/JSX_Concepts/02. JSX_Promise";
-import HandleInput_LocalStorage from "../component/JSX_Concepts/03. JSX_HandleInput";
-import ResponsiveNavbar from "../component/JSX_Projects/03. Navbar";
-import JSX_useEffectRevamp from "../component/JSX_Concepts/04. JSX_useEffect";
-
+import JSX_useEffectRevamp from "../component/JSX_Concepts/02. JSX_Hooks/UseEffect/JSX_useEffect";
 export const JSX_Root = () => {
   return (
     // <div className="flex justify-center items-center">
