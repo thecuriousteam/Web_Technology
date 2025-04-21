f811b0d784e11e306e28d57b0461415ee0e451a3
Curious Team <curiousdevelopers.in@gmail.com>
2025-04-21 08:32:00 +0530

    proper folder structure

--- Backend/index.js
+++ Backend/index.js
@@ -11,25 +11,13 @@ app.use(express.json()); // This will ensure that req.body contains parsed JSON 
 //   next(); // Pass control to the next middleware or route handler
 // }
 
-const {
-  middleware,
-  loggingMiddleware,
-} = require("./ExpressJS/middleware/basic-example");
-
 // Apply the middleware globally to all routes
-app.use(middleware);
-app.use(loggingMiddleware);
 
 // Define the route
 app.get("/", (req, res) => {
   res.send("Hello Express!"); // Send response for the root route
 });
 
-require("./ExpressJS/http-methods/get-example")(app);
-require("./ExpressJS/http-methods/post-example")(app);
-require("./ExpressJS/http-methods/put-example")(app);
-require("./ExpressJS/http-methods/patch-example")(app);
-require("./ExpressJS/http-methods/delete-example")(app);
 // Server configuration
 const PORT = 5001;
 app.listen(PORT, () => {
